ae1119ed5442250ab402230c90f753da6b5c3605
Magnus Pladsen <93226629+MagnusPladsen@users.noreply.github.com>
2023-01-30 21:05:09 +0100

    Started on the carousel on desktop

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ const url = "https://travela.magnuspladsen.no/wp-json/wp/v2/posts?_embed";
 
 const postsContainer = document.querySelector(".posts-container");
 
+function dateFormatter(string) {
+    return string.substring(0, 10);
+}
+
+
 // set up loading message in case of slow connection
 postsContainer.innerHTML = "Loading...";
 
@@ -10,9 +15,10 @@ async function getPosts() {
     const response = await fetch(url);
     const posts = await response.json();
     sessionStorage.setItem("posts", JSON.stringify(posts));
+    displayPosts(posts);
   } catch (error) {
     console.log(error);
-    postsContainer.innerHTML = "Error, please reload the page."
+    postsContainer.innerHTML = "Error, please reload the page.";
   }
 }
 
@@ -25,10 +31,10 @@ function displayPosts(posts) {
     postsContainer.innerHTML += `
           <div class="post-container">
           <h2 class="post-title">${post.title.rendered}</h2>
-          <p class="post-date">${post.date}</p>
+          <p class="post-date">${dateFormatter(post.date)}</p>
           <img
             class="post-img"
-            src="./images/hero.jpeg"
+            src="${post._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url}"
             alt="image of Bali"
           />
         </div>
@@ -41,13 +47,12 @@ if (sessionStorage.getItem("posts")) {
   // if so, get them from sessionStorage
   const posts = JSON.parse(sessionStorage.getItem("posts"));
 
-  // remove loading message
-  postsContainer.innerHTML = "";
-
-  displayPosts(posts);
+  // check is posts already exist in the DOM
+  if (postsContainer) {
+    displayPosts(posts);
+  }
 } else {
   // if not, get them from the API
   getPosts();
   const posts = JSON.parse(sessionStorage.getItem("posts"));
-  displayPosts(posts);
 }
