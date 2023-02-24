7e32c1646039c43010df3680755d347d34d285c0
Magnus Pladsen <93226629+MagnusPladsen@users.noreply.github.com>
2023-02-24 15:16:33 +0100

    Added animation on dropdown menu

--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,14 @@
 const navButton = document.querySelector("#nav-button");
 const dropDown = document.querySelector("#drop-down");
 
-
 if (navButton) {
   navButton.addEventListener("click", () => {
-    if (dropDown.classList.contains("hidden")) {
-      dropDown.classList.remove("hidden");
+    if (dropDown.classList.contains("dropdown-hidden")) {
+      dropDown.classList.remove("dropdown-hidden");
+      dropDown.classList.add("dropdown-show");
+    } else {
+      dropDown.classList.add("dropdown-hidden");
+      dropDown.classList.remove("dropdown-show");
     }
-    else {
-      dropDown.classList.add("hidden");
-    }
-
   });
 }
